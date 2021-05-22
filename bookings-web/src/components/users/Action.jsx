70c539b35251c93b5620942885a07f6aff068e6d
Dominique Sakrisson <domsakrisson@gmailcom>
2021-05-21 17:43:57 -0700

    Update Action

--- bookings-web/src/components/users/Action.jsx
+++ bookings-web/src/components/users/Action.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import {useParams} from 'react-router-dom'
-import styles from '../../styles/styles.css'
 import {userFormChoice} from '../../services/users'
 
 const Action = ({action, onClick}) => {
